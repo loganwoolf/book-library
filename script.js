c8735c8c7f8cb3461509e5f56772643818a368b1
Logan Woolf <loganwoolf@gmail.com>
2021-08-22 23:21:14 -0600

    add read status to class

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 let bookLibrary = []
 
 class Book {
-   constructor(title, author, pages, year) {
+   constructor(title, author, pages, year, status) {
       this.title = title
       this.author = author
       this.pages = pages
       this.year = year
+		this.status = status
    }
    // functions below get added to prototype
    print() {
-      return `'${this.title}' by ${this.author} is ${this.pages} long.`
+		if (this.status) {
+			let readStatement = 'have'
+		} else {
+			let readStatement = 'have not yet'
+		}
+      return `'${this.title}' by ${this.author} is ${this.pages} long. It was published in ${this.year}, and I ${readStatement} read it.`
    }
 
 }
 
-function addBookToLibrary(title, author, pages, year) {
+function addBookToLibrary(title, author, pages, year, status) {
    let book = new Book (
       title,
       author,
       pages,
       year,
+		status,
    )
    bookLibrary.push(book)
    return book
 }
 
 addBookToLibrary("The Lost Colony", 'A. G. Riddle', 380, 2019)
-addBookToLibrary("Shards of Earth", 'Adrian Tchaikovsky', 560, 2021)
+addBookToLibrary("Shards of Earth", 'Adrian Tchaikovsky', 560, 2021, true)
 addBookToLibrary("A Modest Proposal", 'Jonathan Swift', 48, 1729)
 addBookToLibrary('Bird Box', 'Josh Malerman', 262, 2014)
 
@@ -57,6 +64,13 @@ function displayBooks() {
 		year.classList.add('book-year')
 		year.textContent = `Published in ${book.year}`
 		card.appendChild(year)
+
+		const read = document.createElement('div')
+		read.classList.add('book-status')
+		if (book.status) {
+			read.textContent = '📘'
+		}
+		card.appendChild(read)
 	}
 }
 
